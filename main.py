53e488284662cfaf8ac6a8abd662d44b8b8d3d03
egcherepita-rgb <egcherepita@gmail.com>
2025-12-16 23:00:01 +0300

    Update main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import HTMLResponse, StreamingResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.requests import Request
+import io
+
+from parser import build_csv_from_pdf
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/convert")
+async def convert(pdf: UploadFile = File(...)):
+    pdf_bytes = await pdf.read()
+
+    csv_text = build_csv_from_pdf(
+        pdf_bytes=pdf_bytes,
+        items_xlsx_path="data/items.xlsx",
+        delimiter=";"
+    )
+
+    # чтобы Excel в Windows открывал русские буквы нормально:
+    csv_bytes = csv_text.encode("utf-8-sig")
+
+    return StreamingResponse(
+        io.BytesIO(csv_bytes),
+        media_type="text/csv; charset=utf-8",
+        headers={"Content-Disposition": 'attachment; filename="result.csv"'}
+    )
